ae4f60a7e5ab3e5f814ef574de5ddcdde86d9d95
chrisnish4 <chrisnish4@gmail.com>
2024-04-26 00:37:39 -0700

    add beta files

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import json
+import shutil
 from flask import Flask, render_template, request
 from src.predict_pipeline import CustomData, PredictPipeline
 from src.data_ingestion import DataIngestionConfig
+from src.utils import generate_hash, read_gcs # for beta
 
 app = Flask(__name__, template_folder='templates')
 
@@ -40,6 +43,41 @@ def predict_data():
             has_pool=has_pool,
             has_solar=has_solar,
         )
+
+@app.route('/beta_index')
+def beta_index():
+    return render_template('beta_index.html')
+
+@app.route('/beta', methods=["GET", "POST"])
+def beta():
+    if request.method == 'GET':
+        return render_template('beta_index.html')
+    elif request.method == 'POST':
+        address = request.form['address']
+        hash_value = generate_hash(address)
+        object_name = f'{hash_value}.png'
+        local_file_path = f'static/{object_name}'
+        
+        read_gcs('beta-area', object_name, local_file_path)
+        
+        with open('get_beta/beta_bools.json') as f:
+            json_data = json.load(f)
+
+        has_solar = json_data[hash_value]['has_solar']
+        has_pool = json_data[hash_value]['has_pool']
+        return render_template(
+            'beta_result.html',
+            address = address,
+            has_solar = has_solar,
+            has_pool = has_pool,
+            file_name = local_file_path
+        )
+
+#def shutdown_function():
+#    """
+#    Deletes the static folder
+#    """
+#    shutil.rmtree('static')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
